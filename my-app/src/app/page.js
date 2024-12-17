2df1ac51377a8533d3bd16e09473c130b2c4a56d
Halyna Bondar <galya.marusyak@gmail.com>
2024-12-17 20:17:38 +0100

    Add some changes

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -21,6 +21,7 @@ export default function HomePage() {
             router.push(`/ThanksPage?stars=${selectedStars}`);
         }
     }
+
     return (
         <div className={styles.container}>
             <div className={styles.item}>
